f41da0a4132aa45ecaf8b1854ee0d201414aa199
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-05-30 22:29:19 +0000

    game.js added

---
+++ js/game.js
@@ -0,0 +1,11 @@
+/* global Phaser */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 8cd6ed
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
