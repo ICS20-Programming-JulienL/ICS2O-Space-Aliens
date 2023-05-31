228f3e2599706f381d2a64cf3d1eb36b4052e532
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-05-31 21:19:14 +0000

    added a comment to files

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* global Phaser */
 
+// Copyright (c) 2023 Julien Lamoureux inc. All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: May 30 2023
+// This file contains the JS functions for index.html, ICS2O-Space-Aliens
+
+//* Setup for game scene*/
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f8e7a
+  physics : {
+    default : 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set the color of the background
+  backgroundColor: 0x5f8e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // center background scene
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
